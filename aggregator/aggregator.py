2248076ada25a3f067a3e255b86d50638b8624be
Lennard Berger <berger@mecodia.de>
2017-03-01 12:25:21 +0100

    Enhance the API by adding - latest feeds - optional limit parameter

--- aggregator/aggregator.py
+++ aggregator/aggregator.py
@@ -17,7 +17,8 @@ def json_serial(obj):
     raise TypeError ("Type not serializable")
 
 app = Flask(__name__)
-@app.route('/feeds')
+
+@app.route('/feeds/')
 def feeds():
     """
     Returns the list of sources.
@@ -29,15 +30,32 @@ def feeds():
     print(sources)
     return json.dumps(sources)
 
+@app.route('/feeds/latest/')
+@app.route('/feeds/latest/<int:limit>/')
+def latest_feeds(limit=10):
+    """
+    Returns the latest entries for all feeds by limit.
+    """
+    try:
+        latest_feeds = []
+        for entry in Item.select().limit(limit):
+            latest_feeds.append(model_to_dict(entry))
+
+        return json.dumps(latest_feeds, default=json_serial)
+    except IntegrityError:
+        abort(500)
+
+
 @app.route('/feed/<int:id>/')
-def feed_latest(id):
+@app.route('/feed/<int:id>/<int:limit>/')
+def feed_latest(id, limit=10):
     """
     Returns the latest entries for given feed
     """
     try:
         latest_feeds = []
         feed = Source.get(id=id)
-        for entry in Item.select().filter(source=feed).limit(5):
+        for entry in Item.select().filter(source=feed).limit(limit):
             latest_feeds.append(model_to_dict(entry))
 
         return json.dumps(latest_feeds, default=json_serial)
